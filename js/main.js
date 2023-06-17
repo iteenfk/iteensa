6c994896b17c3c9463eb8f6efd2749ee40af61a0
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-06-17 15:12:12 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,48 +1,9 @@
 "use strict"
 
-
-function A(){
-    const x = parseFloat(document.getElementById("x").value);
-    const y = parseFloat(document.getElementById("y").value);
-    const z = x + y;
-    document.getElementById("z").value = z;
+function convertYear() {
+    const yearSelect = document.getElementById("year-select");
+    const selectedYear = parseInt(yearSelect.value);
+    const westernYear = selectedYear + 2018;
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = "西暦: " + westernYear + "年";
 }
-
-
-function S(){
-    const x = parseFloat(document.getElementById("x").value);
-    const y = parseFloat(document.getElementById("y").value);
-    const z = x - y;
-    document.getElementById("z").value = z;
-}
-
-function M(){
-    const x = parseFloat(document.getElementById("x").value);
-    const y = parseFloat(document.getElementById("y").value);
-    const z = x * y;
-    document.getElementById("z").value = z;
-}
-
-function D(){
-    const x = parseFloat(document.getElementById("x").value);
-    const y = parseFloat(document.getElementById("y").value);
-    const z = x / y;
-    document.getElementById("z").value = z;
-}
-
-function T(){
-    const x = parseFloat(document.getElementById("x").value);
-    const y = parseFloat(document.getElementById("y").value);
-    const z = x + y / 2;
-    document.getElementById("z").value = z;
-}
-
-function C(){
-    document.getElementById("x").value=0;
-    document.getElementById("y").value=0;
-    document.getElementById("z").value=0;
-}
-
-
-
-
